2a0b1b1cf78c384bf8db8c02cc05302b5c0b9a2b
janperse <jan@elpy.io>
2018-02-15 12:52:41 +0100

    Fix gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -9,41 +9,40 @@ var browserSync = require('browser-sync'),
 
 // Paths
 var paths = {
-    dist: './dist/',
-    src: './assets/'
+  dist: './dist/',
+  src: './assets/'
 };
 
 // Clean
 gulp.task('clean', require('del').bind(null, [paths.dist]));
 
 // Styles
-gulp.task('styles', function() {
-    return gulp.src(paths.src + 'styles/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoPrefixer({
-            browsers: [
-                'last 2 version',
-                'android 2.3',
-                'android 4',
-                'opera 12'
-            ]
-        }))
-        .pipe(cssNano())
-        .pipe(gulp.dest(paths.dist + 'styles'))
-        .pipe(browserSync.stream());
+gulp.task('styles', function () {
+  return gulp.src(paths.src + 'styles/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoPrefixer({
+        browsers: [
+          'last 2 version',
+          'android 2.3',
+          'android 4',
+          'opera 12'
+        ]
+      }))
+      .pipe(cssNano())
+      .pipe(gulp.dest(paths.dist + 'styles'))
+      .pipe(browserSync.stream());
 });
 
 /* Gulp watch - watch changes of files in 'src' folder (run it by 'gulp watch') */
-gulp.task('watch', function() {
-    browserSync.init({
-        proxy: 'localhost/flexgrid',
-        port: 8080,
-        ui: {
-            port: 8081
-        },
-        files: [
-            './assets/**/*.{scss}'
-        ]
-    });
-    gulp.watch('./assets/styles/**/*.scss', ['styles']);
+gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    },
+    port: 8080,
+    files: [
+      './assets/**/*.{scss}'
+    ]
+  });
+  gulp.watch('./assets/styles/**/*.scss', ['styles']);
 });
